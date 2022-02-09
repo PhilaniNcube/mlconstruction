a7ffe8a7cc23b72fd2027413de0102ec4d37b337
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2022-02-09 13:07:54 +0200

    Update Contact.jsx

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -19,6 +19,7 @@ const Contact = () => {
       console.log(response)
     } catch (error) {
       // toast error message. whatever you wish
+      console.log(error)
       alert('There was error sending the email. You can contact us via telephone on 073 236 4162')
     }
   }
