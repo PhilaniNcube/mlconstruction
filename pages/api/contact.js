3679dd29974f68e05c65c2ec5cfa94c82bf48023
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2022-02-09 13:01:27 +0200

    Update contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -4,20 +4,18 @@ sgMail.setApiKey(process.env.EMAIL_API_KEY)
 
 export default async function handler (req, res) {
   const { message, email, telephone, fullName } = req.body
+  
+  const messageBody = `From: ${fullName} - Email: ${email} - Telephone: ${telephone} - Message: ${message}`
+  
   const msg = {
     to: 'info@maslu.co.za',
-    from: `info@maslu.co.za`,
+    from: 'info@maslu.co.za',
     subject: 'Contact Form',
-    text: `Hello you have received a contact form submission
-    From: ${fullName}
-    Email: ${email}
-    Tel: ${telephone}
-    Message: ${message}
-    `,
+    text: messageBody,
   }
 
  try { 
- const result = await sgMail.send(msg)
+ await sgMail.send(msg)
  res.status(200).json()
  } catch (error) {
   await res.status(500).json({error})
